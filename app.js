dc3495654558c735033ab4630e03d6db132bbebd
Andres <pipecardenast@hotmail.com>
2019-07-23 08:13:04 -0500

    Commit inicial

---
+++ app.js
@@ -0,0 +1,20 @@
+const { argv } = require('./config/yargs');
+const { crearArchivo, listarTabla } = require('./multiplicar/multiplicar');
+const colors = require('colors/safe');
+
+let comando = argv._[0];
+
+switch (comando) {
+  case 'listar':
+    listarTabla(argv.base, argv.limite)
+      .then(resp => console.log(resp))
+      .catch(err => console.log(err));
+    break;
+  case 'crear':
+    crearArchivo(argv.base, argv.limite)
+      .then(archivo => console.log(`Archivo creado: `, colors.green(archivo)))
+      .catch(err => console.log(err));
+    break;
+  default:
+    console.log(`El comando ${comando} no existe`);
+}
---
+++ multiplicar/multiplicar.js
@@ -0,0 +1,59 @@
+const fs = require('fs');
+const colors = require('colors');
+
+let listarTabla = (base, limite = 10) => {
+  return new Promise((resolve, reject) => {
+    if (!Number(base)) {
+      reject(`El valor ingresado ${base}, para la base, no es un número`);
+      return;
+    }
+    if (!Number(limite)) {
+      reject(`El valor ingresado ${limite}, para el límite, no es un número`);
+      return;
+    }
+
+    let data;
+
+    console.log('==================='.green);
+    console.log(`Tabla del ${base}`.green);
+    console.log('==================='.green);
+
+
+    for (let i = 1; i <= limite; i++) {
+      data += `${base} * ${i} = ${base * i}\n`;
+    }
+
+    resolve(data);
+  });
+}
+
+let crearArchivo = (base, limite = 10) => {
+  return new Promise((resolve, reject) => {
+    if (!Number(base)) {
+      reject(`El valor ingresado ${base}, para la base, no es un número`);
+      return;
+    }
+    if (!Number(limite)) {
+      reject(`El valor ingresado ${limite}, para el límite, no es un número`);
+      return;
+    }
+
+    let data = '';
+
+    for (let i = 1; i <= limite; i++) {
+      data += `${base} * ${i} = ${base * i}\n`;
+    }
+
+    fs.writeFile(`tablas/tabla-${base}.txt`, data, (err) => {
+      if (err)
+        reject(err);
+      else
+        resolve(`tabla-${base}.txt`);
+    });
+  });
+}
+
+module.exports = {
+  crearArchivo,
+  listarTabla
+};
